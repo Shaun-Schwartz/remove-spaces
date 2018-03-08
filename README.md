4f20756eccc031762c61562efc897c6c374ca7a1
Shaun Schwartz <me@shaunschwartz.com>
2018-03-07 17:35:19 -0800

    remove pass that was not needed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Simple script to replaces spaces in file names with '.'
+Simple command-line script to replaces spaces in file names with '.'
 
 copy remove_spaces.py, run with python3, and enter the path to the directory housing the files to be renamed
 
--- remove_spaces.py
+++ remove_spaces.py
@@ -4,11 +4,16 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser(description='Replace spaces in file names')
-parser.add_argument('-p', '--path', type=str, help='Specify the path to the directory housing the files to be renamed')
-parser.add_argument('-d', '--directories', action='store_true', default=False, help='Replace spaces in directory names as well as file names')
-parser.add_argument('-r', '--recursive', action='store_true', default=False, help='Recursively replace spaces in file names (and directory names if the -d flag is used)')
-parser.add_argument('-rc', '--replacecharacter', type=str, default='.', help='Specify what character should replace the space in file or directory names (default is .)')
-parser.add_argument('-cr', '--charactertoreplace', type=str, default=' ', help='Specify which character should be replaced (default is space)')
+parser.add_argument('-p', '--path', type=str,
+    help='Specify the path to the directory housing the files to be renamed')
+parser.add_argument('-d', '--directories', action='store_true', default=False,
+    help='Replace spaces in directory names as well as file names')
+parser.add_argument('-r', '--recursive', action='store_true', default=False,
+    help='Recursively replace spaces in file names (and directory names if the -d flag is used)')
+parser.add_argument('-rc', '--replacecharacter', type=str, default='.',
+    help='Specify what character should replace the space in file or directory names (default is .)')
+parser.add_argument('-cr', '--charactertoreplace', type=str, default=' ',
+    help='Specify which character should be replaced (default is space)')
 args = parser.parse_args()
 
 d = args.directories
@@ -30,7 +35,6 @@ if args.recursive:
         for file in files:
             if os.path.isfile(os.path.join(folder + file)):
                 print('This is a file {}'.format(folder + file))
-
 else:
     if args.directories:
         for file in os.listdir(path):
@@ -52,4 +56,3 @@ else:
                     print('File renamed to: {}'.format(path + new_name))
                 except:
                     print('Couldn\'t rename file')
-                    pass
